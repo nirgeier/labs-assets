eca99b896206cf0a0e208e0160cbd90938dbe761
Nir Geier <nirgeier@gmail.com>
2024-10-12 20:48:58 +0300

    Initial commit

---
+++ assets/scripts/colors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Reset
+COLOR_OFF='\033[0m'         # Text Reset
+NO_COLOR='\033[0m'          # Text Reset
+
+# Regular Colors
+BLACK='\033[0;30m'          # Black
+RED='\033[0;31m'            # Red
+GREEN='\033[0;32m'          # Green
+YELLOW='\033[0;33m'         # Yellow
+BLUE='\033[0;34m'           # Blue
+PURPLE='\033[0;35m'         # Purple
+CYAN='\033[0;36m'           # Cyan
+WHITE='\033[0;37m'          # White
+
+# Bold
+BOLD_BLACK='\033[1;30m'     # Black
+BOLD_RED='\033[1;31m'       # Red
+BOLD_GREEN='\033[1;32m'     # Green
+BOLD_YELLOW='\033[1;33m'    # Yellow
+BOLD_BOLD_LUE='\033[1;34m'  # Blue
+BOLD_PURPLE='\033[1;35m'    # Purple
+BOLD_CYAN='\033[1;36m'      # Cyan
+BOLD_WHITE='\033[1;37m'     # White
